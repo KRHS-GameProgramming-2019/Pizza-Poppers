4bbff672c663dfed558a8384c3d23d0b901c257c
jchambers24 <jtcroxley@gmail.com>
2019-12-09 11:25:39 -0500

    Added a tileMap

--- TileMap.py
+++ TileMap.py
@@ -1,14 +1,38 @@
 import pygame, random, math
-
+from Tile import *
+tm = [
+"###########",
+"#---------#",
+"#---------#",
+"#---------#",
+"#---------#",
+"#---------#",
+"#---------#",
+"#---------#",
+"#---------#",
+"###########",
+]
 class TileMap:
-	def __init__(self, size, level):
-		self.size = size
-		self.tiles = []
-		self.level = level
-		self.buildMap()
+    def __init__(self, size, level):
+        self.size = size
+        self.tiles = []
+        self.level = level
+        self.loadingMap = False
+        self.buildMap()
 
-	def buildMap(self):
-		self.levelFile = open("Levels/"+self.level+".lvl")
-		levelData = self.levelFile.readlines()
-		for x in levelData:
-			print(x)
+    def buildMap(self):
+        y = -1
+        for line in tm:
+            x=-1
+            y+=1
+            for char in line:
+                x+=1
+                print(x,y)
+                if char == "#":
+                    self.tiles.append(Tile((x*80,y*80), "Images/Tiles/counter.png", False, False))
+                if char == "-":
+                    self.tiles.append(Tile((x*80,y*80), "Images/Tiles/floor.png", False, False))
+        
+    def render(self,screen):
+        for tile in self.tiles:
+            screen.blit(tile.image, tile.rect)
