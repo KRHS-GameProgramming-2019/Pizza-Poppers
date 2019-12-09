ee722563d9de3fa2788319b06d3cfe75f8a6d2ae
jchambers24 <jtcroxley@gmail.com>
2019-12-09 11:29:38 -0500

    Fixes

--- Main.py
+++ Main.py
@@ -2,11 +2,11 @@ import pygame, TileMap
 pygame.init()
 screen = pygame.display.set_mode((880,800))
 clock = pygame.time.Clock()
-tm = TileMap.TileMap(10,"test")
+tileMap = TileMap.TileMap(10,"test")
 while True:
     #  Main Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit();
-    tm.render(screen)
+    tileMap.render(screen)
     pygame.display.flip()
