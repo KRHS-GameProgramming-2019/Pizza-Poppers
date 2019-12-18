b3da4f993183b97351232545ae16582ba6634cca
Jack Chambers <jtcroxley@gmail.com>
2019-12-18 11:30:55 -0500

    Added floor for optimization

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
 import pygame, TileMap, LevelHandler
 from Player import *
 pygame.init()
-screen = pygame.display.set_mode((960,880))
+screen = pygame.display.set_mode((1260,880))
 pygame.display.set_caption("Pizza Poppers")
 icon = pygame.image.load("Images/icon.png")
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 tileMap = TileMap.TileMap(10,"test.lvl")
 plr = Player(5,(100,100))
+bkg = pygame.image.load("Images/Backgrounds/floor.png")
 
 while True:
     #  Main Event Loop
@@ -19,6 +20,7 @@ while True:
     
     #rendering
     screen.fill((0,0,0))
+    screen.blit(bkg,(0,0))
     tileMap.render(screen)
     screen.blit(plr.image,plr.rect)
     pygame.display.flip()
