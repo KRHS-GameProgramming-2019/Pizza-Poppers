22164d85608730118d7560cc952090f4d315a527
Jack Chambers <jtcroxley@gmail.com>
2019-12-19 11:37:59 -0500

    Added Order Conveyor

--- Main.py
+++ Main.py
@@ -10,7 +10,20 @@ tileMap = TileMap.TileMap(10,"test.lvl")
 plr = Player(5,(100,100))
 bkg = pygame.image.load("Images/Backgrounds/floor.png")
 
+conveyors = [
+pygame.image.load("Images/Backgrounds/Conveyor3.png"),
+pygame.image.load("Images/Backgrounds/Conveyor2.png"),
+pygame.image.load("Images/Backgrounds/Conveyor1.png")
+]
+
+cvRect = conveyors[0].get_rect()
+cvRect.x = 960
+count = 0
+frameCount = 0
 while True:
+    frameCount += 1
+    if count == 2:
+        count = 0
     #  Main Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,5 +36,8 @@ while True:
     screen.blit(bkg,(0,0))
     tileMap.render(screen)
     screen.blit(plr.image,plr.rect)
+    if frameCount%5 == 0:
+        count += 1
+    screen.blit(conveyors[count], cvRect)
     pygame.display.flip()
     clock.tick(60)
