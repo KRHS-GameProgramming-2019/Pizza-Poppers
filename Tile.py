2153ec61d92341c0294b5c0d307672a47b5a4c85
Jack Chambers <jtcroxley@gmail.com>
2019-12-19 10:55:07 -0500

    Fixed tile stuff

--- Tile.py
+++ Tile.py
@@ -1,13 +1,13 @@
 import pygame, random
 
 class Tile:
-    def __init__(self, position, image, rot, canCollide=False, canInteract=False):
+    def __init__(self, position, image, canCollide=False, canInteract=False):
         self.position = position
-        self.rot = rot
+        #self.rot = rot
         self.image = pygame.image.load(image)
         self.canCollide = canCollide
         self.canInteract = canInteract
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
         self.rect.y = position[1]
-        self.image = pygame.transform.rotate(self.image, self.rot)
+        #self.image = pygame.transform.rotate(self.image, self.rot)
