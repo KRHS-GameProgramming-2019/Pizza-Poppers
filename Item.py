620a2bface61b0da42e8778520a4382e6bc84065
Jack Chambers <jtcroxley@gmail.com>
2020-01-10 11:36:50 -0500

    Added item system tweaks

--- Item.py
+++ Item.py
@@ -5,6 +5,7 @@ class Item:
         self.location = location
         self.itemType = itemType
         self.image = pygame.image.load("Images/Items/"+self.itemType+".png")
+        self.rect = self.image.get_rect()
 
     def update(self, parent):
         self.position = [parent.rect.x, parent.rect.y]
--- ItemHandler.py
+++ ItemHandler.py
@@ -6,10 +6,15 @@ def sendItem(item,other):
     otherItem = other.holding
     other.holding = item
 def deleteItem(item):
-	count  = 0
-	for i in items:
-		count += 1
-		if i == item:
-			items.remove(count)
-def createItem(itemType):
-	Item()
+    count  = 0
+    for i in items:
+        count += 1
+        if i == item:
+            items.remove(count)
+def createItem(pos, itemType):
+    newItem = Item(pos, itemType)
+    items.append(newItem)
+
+def renderItems(screen):
+    for item in items:
+        screen.blit(item.image, item.rect)
