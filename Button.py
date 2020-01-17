76ade71e913dc875f45ba62d86511b857e93ce51
Jack Chambers <jtcroxley@gmail.com>
2020-01-17 11:37:27 -0500

    Added Buttons and button template

--- Button.py
+++ Button.py
@@ -1,17 +1,19 @@
-import pygame
+import pygame, os
 
 class Button:
     def __init__(self, startPos, bType):
         self.state = 0
-        self.images = [pygame.image.load("Images\Buttons\Play\Button.png"), pygame.image.load("Images\Buttons\Play\ButtonHover.png"), pygame.image.load("Images\Buttons\Play\ButtonClick.png")]
+        self.bType = bType
+        self.images = [pygame.image.load(os.path.join("Images\Buttons",bType,"Button.png")), pygame.image.load(os.path.join("Images\Buttons",bType,"ButtonHover.png")), pygame.image.load(os.path.join("Images\Buttons",bType,"ButtonClick.png"))]
         self.rect = self.images[0].get_rect()
         self.rect.x = startPos[0]
         self.rect.y = startPos[1]
     
-    def update(self):
+    def update(self, var=None):
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             if pygame.mouse.get_pressed()[0]:
                 self.state = 2
+                return True
             else:
                 self.state = 1
         else:
--- Main.py
+++ Main.py
@@ -1,33 +1,55 @@
-import pygame, TileMap, LevelHandler, Console, ItemHandler
+import pygame, Console, TileMap
+from ItemHandler import *
 from Player import *
 from Conveyor import *
+from Button import *
+
+# Window Setup
 pygame.init()
 screen = pygame.display.set_mode((1260,880))
 pygame.display.set_caption("Pizza Poppers")
 icon = pygame.image.load("Images/icon.png")
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
+screenState = "Title"
+
+# Game Objects
 tileMap = TileMap.TileMap(10,"test.lvl")
 plr = Player(5,(100,100))
 bkg = pygame.image.load("Images/Backgrounds/Floor1.png")
 cv = Conveyor(960,800)
+
+# Title Screen Objects
+title = pygame.image.load("Images/Backgrounds/TitleScreen1.png")
+play = Button((100,250),"Play")
+leave = Button((100,350),"Template")
 
 while True:
     #  Main Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit();
-
-    #input & calcs
-    plr.get_input()
+            
+    # Rendering
     Console.listen()
-    
-    #rendering
     screen.fill((0,0,0))
-    screen.blit(bkg,(0,0))
-    cv.animate(screen)
-    tileMap.render(screen)
-    screen.blit(plr.image,plr.rect)
-    ItemHandler.renderItems(screen)
+    
+    if screenState == "Play": # Game Window
+        plr.get_input()
+        screen.blit(bkg,(0,0))
+        cv.animate(screen)
+        tileMap.render(screen)
+        screen.blit(plr.image,plr.rect)
+        renderItems(screen)
+        
+    if screenState == "Title": # Title Screen Window
+        screen.blit(title,(0,0))
+        if play.update():
+            screenState = "Play"
+        if leave.update():
+            # ~ exit();
+            pass
+        leave.render(screen)
+        play.render(screen)
     pygame.display.flip()   
     clock.tick(60)
